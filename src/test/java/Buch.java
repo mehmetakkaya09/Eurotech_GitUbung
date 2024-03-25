76bc5ef87d1cf34be0334845a49fbbe8e987604c
MehmetAkkaya <mehmetakkaya1990@gmail.com>
2024-03-25 18:11:37 +0300

    letzte Veranderungen

--- src/test/java/Buch.java
+++ src/test/java/Buch.java
@@ -8,7 +8,7 @@ public class Buch {
 
         System.out.println("Mehmet hat neue Veranderungen");
 
-        System.out.println("Sumeyra hat letzte Veranderungen für Konflikt");
+        System.out.println("Mehmet hat letzte Veranderungen für Konflikt");
 
 
 
